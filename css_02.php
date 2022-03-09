55d936ae257157623a952e54dd1002b8b692c82f
Wendel Macedfo <96504832+Wendelzin99@users.noreply.github.com>
2022-03-08 21:40:10 -0300

    20220308_01

---
+++ css_02.php
@@ -0,0 +1,21 @@
+<?php require __DIR__ . "/_header.php"; ?>
+
+<style>
+.topo {
+    background-color: red;
+}
+
+</style>
+
+<div class="topo">coisas</div>
+<div class="conteudo">
+    <h5>Título</h5>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis iste, neque quasi doloribus adipisci maiores. Facere, doloremque. Praesentium sed ratione delectus veritatis, accusantium, in sit ad sequi rem cumque error?</p>
+    <h4>Título 4</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam earum eveniet, doloremque minus odit illo rerum nulla dicta, quibusdam quis culpa, aperiam perspiciatis iure modi eius molestias aspernatur blanditiis excepturi?</p>
+    <blockquote>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem voluptas accusantium, aliquid, ad inventore sit veniam fugit ut quod unde, odit sequi provident nihil accusamus nulla alias fuga quaerat doloremque!
+    </blockquote>
+</div>
+
+<?php require __DIR__ . "/_footer.php"; ?>
